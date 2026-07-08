05331f22ce74a676fb4279548dd004e23df2f70c
khushishimpi11 <khushishimpi1119@gmail.com>
2026-07-08 13:34:35 +0530

    changes done

--- vite.config.ts
+++ vite.config.ts
@@ -11,54 +11,54 @@ export default defineConfig(({ mode }) => {
   const apiUrl = isProduction
     ? "https://jewelskart-backend-gt7z.onrender.com/api"
     : "http://localhost:5000/api";
-  
+
   process.env.VITE_API_URL = apiUrl;
 
   return {
     server: {
-    host: "::",
-    port: 8080,
-  },
-
-  plugins: [
-    react(),
-
-    mode === "development" && componentTagger(),
-
-    // ✅ PWA Plugin
-    VitePWA({
-      registerType: "autoUpdate",
-
-      manifest: {
-        name: "JewelsKart",
-        short_name: "JewelsKart",
-        description: "Premium Jewellery Shopping App 💎",
-        theme_color: "#540f29",
-        background_color: "#ffffff",
-        display: "standalone",
-        orientation: "portrait",
-        start_url: "/",
-
-        icons: [
-          {
-            src: "/logo1.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "/logoicon.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
-  ].filter(Boolean),
-
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+      host: "::",
+      port: 8080,
     },
-  },
+
+    plugins: [
+      react(),
+
+      mode === "development" && componentTagger(),
+
+      // ✅ PWA Plugin
+      VitePWA({
+        registerType: "autoUpdate",
+
+        manifest: {
+          name: "JewelsKart",
+          short_name: "JewelsKart",
+          description: "Premium Jewellery Shopping App 💎",
+          theme_color: "#540f29",
+          background_color: "#ffffff",
+          display: "standalone",
+          orientation: "portrait",
+          start_url: "/",
+
+          icons: [
+            {
+              src: "/logo1.jpg",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "/logoicon.png",
+              sizes: "512x512",
+              type: "image/png",
+            },
+          ],
+        },
+      }),
+    ].filter(Boolean),
+
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
   };
 });
